e9bad363b34aad5c8e194a54cd93e4b113a26533
MouseTrap-codes <mousetrapcodes@gmail.com>
2025-02-13 02:51:01 -0600

    added functionality for zhash (should work)

--- build.sh
+++ build.sh
@@ -1,23 +1,4 @@
 #!/bin/bash
 
 # Compiler and flags
-CC=gcc
-CFLAGS="-Wall -Wextra -std=c11 -O2"
-LDFLAGS="-lczmq"  # Link against czmq library
-
-# Output binary name
-OUTFILE="tinker_assembler"
-
-# Source files (add more if needed)
-SRC="assembler.c"
-
-# Compile the program
-echo "Compiling..."
-$CC $CFLAGS $SRC -o $OUTFILE $LDFLAGS
-
-# Check if compilation was successful
-if [ $? -eq 0 ]; then
-    echo "Build successful! Run ./tinker_assembler <input.tk> <output.bin>"
-else
-    echo "Build failed!"
-fi
+gcc -o hw5 main.c 
